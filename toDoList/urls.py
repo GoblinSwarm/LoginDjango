126372304a62190196006fe7cdae063901c23082
Daniel Perdomo <daniel.perdomo.1987@gmail.com>
2025-04-18 22:30:05 -0300

    Tareas agregadas por usuario, login funcional y logout funcional

--- toDoList/urls.py
+++ toDoList/urls.py
@@ -1,8 +1,11 @@
 from django.urls import path
-from .views import create_task, login_register_view, task_view
+from django.contrib.auth import views as auth_views
+from .views import create_task_view, login_register_view, task_view, toggle_task_status_view
 
 urlpatterns = [
     path('', login_register_view, name='login'),
     path('show_tasks/', task_view, name='show-tasks'),
-    path('create_task/', create_task, name='create-task'),
+    path('create_task/', create_task_view, name='create-task'),
+    path('toggle_task/<int:task_id>/', toggle_task_status_view, name='toggle-task'),
+    path('logout/', auth_views.LogoutView.as_view(), name='logout'),
 ]
--- toDoList/views.py
+++ toDoList/views.py
@@ -1,4 +1,5 @@
-from django.shortcuts import render, redirect, reverse
+from django.shortcuts import render, redirect, reverse, get_object_or_404
+from django.contrib.auth.decorators import login_required
 from django.contrib.auth import authenticate, login, logout
 from .forms import LoginForm, RegisterForm
 from django.contrib.auth.models import User
@@ -13,28 +14,42 @@ def login_register_view(request):
     if 'login_submit' in request.POST and login_form.is_valid():
         user = login_form.get_user()
         login(request, user)
-        #En este caso hay que modificarlo para que redirija a la vista de tareas
-        return redirect('show-tasks')
+        return redirect(reverse('show-tasks'))
     
     if 'register_submit' in request.POST and register_form.is_valid():
         user = register_form.save()
         login(request, user)
-        #En este caso hay que modificarlo para que redirija a la vista de tareas
-        return redirect('show-tasks')
+        return redirect(reverse('show-tasks'))
     
     return render(request, 'toDoList/login_register.html', {
         'login_form': login_form,
         'register_form': register_form,
         })
 
+@login_required
 def task_view(request):
-    tasks = Task.objects.filter(user=request.user).order_by('-creation_date')
+    tasks = Task.objects.filter(user=request.user).order_by('-creation_date').order_by('completed')
     return render(request, 'toDoList/show_tasks.html', {'tasks': tasks})
 
-
-def create_task(request):
-    if request.POST:
-        new_task = request.POST.get('tarea')
-        Task.objects.create(description=new_task, done=False)
-        return redirect(reverse('create-task'))
+@login_required
+def create_task_view(request):
+    if request.method == 'POST':
+        new_task_title = request.POST.get('tarea')
+        if new_task_title:
+            Task.objects.create(
+                user=request.user,
+                title=new_task_title,
+                completed=False
+            )
+            return redirect('show-tasks')
     return render(request, 'toDoList/create_task.html')
+
+@login_required
+def toggle_task_status_view(request, task_id):
+    task = get_object_or_404(Task, id=task_id, user=request.user)
+    
+    if request.method == 'POST':
+        task.completed = not task.completed
+        task.save()
+
+    return redirect('show-tasks')
